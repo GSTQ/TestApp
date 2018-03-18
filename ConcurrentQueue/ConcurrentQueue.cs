138853970e8d1e856f22029412e0d8551470380d
gstq <ramazanov.anvar@gmail.com>
2018-03-18 23:36:38 +0500

    Add concurent queue task

---
+++ ConcurrentQueue/ConcurrentQueue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentQueue
+{
+    public class ConcurentQueue<T>
+    {
+        public ConcurentQueue()
+        {
+            _data = new Queue<T>();
+        }
+
+        public void Push(T item)
+        {
+            lock (_data)
+            {
+                _data.Enqueue(item);
+            }
+        }
+
+        public async Task<T> Pop()
+        {
+            while (_data.Count == 0)
+            {
+                await Task.Delay(200);
+            }
+            lock (_data)
+            {
+                return _data.Dequeue();
+            }
+        }
+
+        private Queue<T> _data;
+    }
+}
---
+++ ConcurrentQueue/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConcurrentQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestAsync();
+            Console.ReadKey();
+        }
+
+        private static async void TestAsync()
+        {
+            var queue = new ConcurentQueue<String>();
+
+            await Task.Factory.StartNew(async () =>
+            {
+                var data = await queue.Pop();
+                Console.WriteLine(data);
+            });
+
+            await Task.Factory.StartNew(async () =>
+            {
+                var data = await queue.Pop();
+                Console.WriteLine(data);
+            });
+
+            await Task.Factory.StartNew(async () =>
+            {
+                var data = await queue.Pop();
+                Console.WriteLine(data);
+            });
+
+            await Task.Factory.StartNew(async () =>
+            {
+                var data = await queue.Pop();
+                Console.WriteLine(data);
+            });
+
+            await Task.Factory.StartNew(async () =>
+            {
+                var data = await queue.Pop();
+                Console.WriteLine(data);
+            });
+
+            await Task.Factory.StartNew(() =>
+            {
+                queue.Push("Task1");
+            });
+
+            await Task.Factory.StartNew(() =>
+            {
+                queue.Push("Task2");
+            });
+
+            await Task.Factory.StartNew(() =>
+            {
+                queue.Push("Task3");
+            });
+
+            await Task.Factory.StartNew(() =>
+            {
+                queue.Push("Task4");
+            });
+
+            await Task.Factory.StartNew(() =>
+            {
+                queue.Push("Task5");
+            });
+        }
+    }
+}
