69dc75cd173ba3cdf75a4132adec2513996d5d96
gstq <ramazanov.anvar@gmail.com>
2018-03-18 23:36:55 +0500

    Add array pairs task

---
+++ ArrayPairs/ArrayPairs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ArrayPairs
+{
+    public class ArrayPairs
+    {
+        public static void PrintPairSums(int[] array, int sum)
+        {
+            Array.Sort(array);
+            var first = 0;
+            var last = array.Length - 1;
+            while (first < last)
+            {
+                var s = array[first] + array[last];
+                if (s == sum)
+                {
+                    Console.WriteLine($"{array[first]}  {array[last]}");
+                    first++;
+                    last--;
+                }
+                else
+                {
+                    if (s < sum)
+                    {
+                        first++;
+                    }
+                    else
+                    {
+                        last--;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ArrayPairs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArrayPairs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("All pairs for (1, 1, 2, 1, 1, 0, 1), sum = 2");
+            ArrayPairs.PrintPairSums(new int[] { 1, 1, 2, 1, 1, 0, 1 }, 2);
+            Console.WriteLine("All pairs for (1, 1, 2, 1, 1, 0, 1), sum = 3");
+            ArrayPairs.PrintPairSums(new int[] { 1, 1, 2, 1, 1, 0, 1 }, 3);
+            Console.WriteLine("All pairs for (1, 1, 2, 3, 4, 2, 1, 0), sum = 4");
+            ArrayPairs.PrintPairSums(new int[] { 1, 1, 2, 3, 4, 2, 1, 0 }, 4);
+            Console.ReadKey();
+        }
+    }
+}
